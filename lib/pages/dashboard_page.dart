520598f987f9a3bf9933395cd6868e53d9a705b6
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-05-23 14:02:19 +0700

    Custom App Bar

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -8,8 +10,53 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Color(0xffF2F6FD),
+      body: Container(
+        margin: EdgeInsets.only(top: 8),
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: [
+            // Custom App Bar
+            Container(
+              margin: EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // hamburger bar
+                    SvgPicture.asset('assets/svg/Icon_menu_outline.svg'),
+                    Container(
+                        margin: EdgeInsets.only(right: 98, top: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Hello,",
+                              style: GoogleFonts.poppins(fontSize: 22),
+                            ),
+                            Text(
+                              "Shayna Far",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 22, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        )),
+                        // image user
+                    Container(
+                      margin: EdgeInsets.only(top: 19),
+                      height: 69,
+                      width: 69,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/user_pic.png'),
+                        ),
+                      ),
+                    ),
+                  ]),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
