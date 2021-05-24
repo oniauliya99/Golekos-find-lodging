077bd7cd821b66cb54bee53528bbd008d39a996b
oniauliya99 <oniauliya99@gmail.com>
2021-05-24 23:27:38 +0700

    Create sign_up_page.dart

---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:golekos/theme.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
