b3105c684b05a7ee219c27ecfadac34c8c83b206
oniauliya99 <oniauliya99@gmail.com>
2021-05-24 23:23:43 +0700

    Update view login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Color(0xFFF2F6FD),
       body: Container(
-        margin: EdgeInsets.only(top: 150),
+        margin: EdgeInsets.only(top: 100),
         child: ListView(
           physics: ClampingScrollPhysics(),
           children: [
@@ -21,7 +21,7 @@ class _LoginPageState extends State<LoginPage> {
               alignment: Alignment.center,
               child: Text(
                 'Sign In',
-                style: orderRegular.copyWith(fontSize: 28),
+                style: orderRegular.copyWith(fontSize: 36),
               ),
             ),
             Container(
@@ -41,17 +41,16 @@ class _LoginPageState extends State<LoginPage> {
                             Container(
                               decoration: BoxDecoration(
                                 image: DecorationImage(
-                                  image: AssetImage(
-                                      'assets/images/sign_in/logo.png'),
+                                  image: AssetImage('assets/images/tips.png'),
                                 ),
                               ),
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 90),
-                              child: Text(
-                                'Sign in as Guest ',
-                                style: orderRegular,
-                              ),
+                              margin: EdgeInsets.only(left: 50),
+                              child: Text('Sign in as Guest ',
+                                  style: orderRegular.copyWith(
+                                    fontSize: 20,
+                                  )),
                             ),
                           ],
                         ),
@@ -84,11 +83,11 @@ class _LoginPageState extends State<LoginPage> {
                               ),
                             ),
                             Container(
-                              margin: EdgeInsets.only(left: 80),
+                              margin: EdgeInsets.only(left: 50),
                               child: Text(
                                 'Sign in with Google ',
-                                style:
-                                    orderRegular.copyWith(color: Colors.blue),
+                                style: orderRegular.copyWith(
+                                    color: Color(0XFF4285F4), fontSize: 20),
                               ),
                             ),
                           ],
@@ -104,7 +103,7 @@ class _LoginPageState extends State<LoginPage> {
               margin: EdgeInsets.only(top: 30),
               child: Text(
                 'or get a link emailed to you',
-                style: orderRegular.copyWith(fontSize: 11, color: Colors.grey),
+                style: orderRegular.copyWith(fontSize: 15, color: Colors.grey),
               ),
             ),
             new ListTile(
@@ -119,13 +118,76 @@ class _LoginPageState extends State<LoginPage> {
                   alignment: Alignment.center,
                   child: new TextFormField(
                     keyboardType: TextInputType.text,
+                    style: orderRegular.copyWith(fontSize: 20),
                     decoration: InputDecoration(
                         hintText: 'Work email address',
-                        hintStyle: orderRegular,
+                        hintStyle: orderRegular.copyWith(fontSize: 20),
                         border: InputBorder.none),
                     onChanged: (value) {},
                   ),
                 ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40, top: 10),
+              height: 65,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: Color(0XFF29D5F8),
+                child: ListTile(
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Text(
+                          'Email me a sign up link ',
+                          style: orderRegular.copyWith(
+                              color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 30),
+                  child: Text(
+                    'or register now ',
+                    style: orderSemiBold.copyWith(
+                        fontSize: 15, color: Colors.grey),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 30),
+                  child: Text(
+                    'here ',
+                    style:
+                        orderRegular.copyWith(fontSize: 15, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 80),
+              child: Text(
+                'You are completely safe',
+                style: orderBold.copyWith(fontSize: 15, color: Colors.black),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Read Our Terms & Conditions',
+                style: orderRegular.copyWith(
+                    fontSize: 15, color: Color(0XFF7041EE)),
               ),
             ),
           ],
