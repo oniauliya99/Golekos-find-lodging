afb2ee35ec5c51f5339b6fdcb7ba29d8a9964387
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-05-24 20:43:31 +0700

    Add app bar image & Container for content

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -8,8 +8,37 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Color(0xffF2F6FD),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 420,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/pic2.png'),
+              ),
+            ),
+          ),
+          ListView(
+            // padding: EdgeInsets.zero,
+            children: [
+              SizedBox(
+                height: 350,
+              ),
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                  color: Color(0xffF2F6FD),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
