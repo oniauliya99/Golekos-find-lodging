449fd819cea81cc71216ae614437a1eff678df39
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-05-27 14:51:17 +0700

    add icon button & modified body

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailPage extends StatefulWidget {
   @override
@@ -12,30 +13,53 @@ class _DetailPageState extends State<DetailPage> {
       backgroundColor: Color(0xffF2F6FD),
       body: Stack(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 420,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage('assets/images/pic2.png'),
+          Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 360,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/pic2.png'),
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          icon: SvgPicture.asset(
+                            'assets/svg/btn_back.svg',
+                            width: 50,
+                          ),
+                          onPressed: () {}),
+                      IconButton(
+                          icon: SvgPicture.asset(
+                            'assets/svg/btn_love.svg',
+                            width: 50,
+                          ),
+                          onPressed: () {}),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(22),
+              child: ListView(),
+              margin: EdgeInsets.only(top: 318),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30),
+                ),
+                color: Color(0xffF2F6FD),
               ),
             ),
-          ),
-          ListView(
-            // padding: EdgeInsets.zero,
-            children: [
-              SizedBox(
-                height: 350,
-              ),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-                  color: Color(0xffF2F6FD),
-                ),
-              )
-            ],
           )
         ],
       ),
