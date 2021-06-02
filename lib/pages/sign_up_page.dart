f7a5df1befd8a4c3be53b377f5507dc6643cbe8c
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-06-02 20:51:23 +0700

    Add controller & modified UI

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:golekos/theme.dart';
+import 'package:golekos/services/signin.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -7,15 +8,30 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF2F6FD),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+      ),
+      backgroundColor: Color(0xFFFFAFBFD),
       body: Container(
-        margin: EdgeInsets.only(top: 100),
+        margin: EdgeInsets.only(top: 10),
         child: ListView(
           physics: ClampingScrollPhysics(),
           children: [
+            Container(
+                height: 110,
+                margin: EdgeInsets.only(bottom: 30),
+                alignment: Alignment.center,
+                child: Image.asset("assets/images/golekos_logo.png")),
             Container(
               margin: EdgeInsets.only(bottom: 30),
               alignment: Alignment.center,
@@ -26,56 +42,43 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             Container(
               decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                  color: Color(0xFFEEEEEE),
+                  borderRadius: BorderRadius.circular(20)),
               margin: EdgeInsets.only(left: 40, right: 40, bottom: 17),
               height: 60,
               child: Container(
                 margin: EdgeInsets.only(left: 20),
                 alignment: Alignment.center,
                 child: new TextFormField(
-                  keyboardType: TextInputType.text,
+                  controller: email,
                   style: orderRegular.copyWith(fontSize: 20),
                   decoration: InputDecoration(
-                      hintText: 'Phone or email address',
-                      hintStyle: orderRegular.copyWith(fontSize: 20),
-                      border: InputBorder.none),
+                    hintText: 'Email address',
+                    hintStyle: orderRegular.copyWith(
+                        fontSize: 20, color: Color(0x6C383737)),
+                    border: InputBorder.none,
+                  ),
                   onChanged: (value) {},
                 ),
               ),
             ),
             Container(
               decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                  color: Color(0xFFEEEEEE),
+                  borderRadius: BorderRadius.circular(20)),
               margin: EdgeInsets.only(left: 40, right: 40, bottom: 17),
               height: 60,
               child: Container(
                 margin: EdgeInsets.only(left: 20),
                 alignment: Alignment.center,
                 child: new TextFormField(
-                  keyboardType: TextInputType.text,
+                  controller: password,
+                  obscureText: true,
                   style: orderRegular.copyWith(fontSize: 20),
                   decoration: InputDecoration(
                       hintText: 'Password',
-                      hintStyle: orderRegular.copyWith(fontSize: 20),
-                      border: InputBorder.none),
-                  onChanged: (value) {},
-                ),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              margin: EdgeInsets.only(left: 40, right: 40, bottom: 17),
-              height: 60,
-              child: Container(
-                margin: EdgeInsets.only(left: 20),
-                alignment: Alignment.center,
-                child: new TextFormField(
-                  keyboardType: TextInputType.text,
-                  style: orderRegular.copyWith(fontSize: 20),
-                  decoration: InputDecoration(
-                      hintText: 'Confirm Password',
-                      hintStyle: orderRegular.copyWith(fontSize: 20),
+                      hintStyle: orderRegular.copyWith(
+                          fontSize: 20, color: Color(0x6C383737)),
                       border: InputBorder.none),
                   onChanged: (value) {},
                 ),
@@ -92,31 +95,42 @@ class _SignUpPageState extends State<SignUpPage> {
             Container(
               margin: EdgeInsets.only(left: 40, right: 40, top: 10),
               height: 65,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Color(0XFF29D5F8),
-                child: ListTile(
-                  title: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Text(
-                          'Sign up now',
-                          style: orderRegular.copyWith(
-                            color: Colors.white,
-                            fontSize: 20,
+              child: InkWell(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color(0XFF29D5F8),
+                  child: ListTile(
+                    title: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            'Sign up now',
+                            style: orderRegular.copyWith(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
+                onTap: () async {
+                  signUp(email.text, password.text).then(
+                    (result) {
+                      if (result != null) {
+                        Navigator.pop(context);
+                      }
+                    },
+                  );
+                },
               ),
             ),
             Container(
               alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 130),
+              margin: EdgeInsets.only(top: 110),
               child: Text(
                 'You are completely safe',
                 style: orderBold.copyWith(fontSize: 15, color: Colors.black),
