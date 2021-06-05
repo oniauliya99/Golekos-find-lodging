7062261fe6edd67fb32ec2202f0c53f241286b65
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-06-05 13:33:16 +0700

    Change Buttom bar

--- lib/pages/buttom_bar.dart
+++ lib/pages/buttom_bar.dart
@@ -1,48 +1,74 @@
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:golekos/models/product.dart';
 import 'package:golekos/pages/dashboard_page.dart';
-import 'package:golekos/pages/home_page.dart';
 import 'package:golekos/pages/profile_page.dart';
 
 class ButtomBar extends StatefulWidget {
+  final User user;
+  ButtomBar({this.user});
   @override
   _ButtomBarState createState() => _ButtomBarState();
 }
 
 class _ButtomBarState extends State<ButtomBar> {
   int _currentIndex = 0;
+  List<Widget> _listTab;
 
-  List<Widget> _widgetOptions = [
-    Dashboard(),
-    Profile(),
-  ];
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _listTab = [
+      Dashboard(widget.user),
+      Profile(),
+    ];
+  }
+
+  // Get data from API
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _widgetOptions.elementAt(_currentIndex),
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: _currentIndex,
-        onItemSelected: _onItemTapped,
-        items: [
-          BottomNavyBarItem(
-            icon: SvgPicture.asset("assets/svg/Icon_home_solid.svg"),
-            title: Text("HOME"),
-            activeColor: Color(0xff2DC6F2),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 55.0),
+            child: _listTab[_currentIndex],
           ),
-          BottomNavyBarItem(
-            icon: SvgPicture.asset("assets/svg/Icon_user_solid.svg"),
-            title: Text("PROFILE"),
-            activeColor: Color(0xff2DC6F2),
-          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavigationBar(
+                items: _bottomNavItem,
+                currentIndex: _currentIndex,
+                selectedItemColor: Color(0xff2DC6F2),
+                unselectedItemColor: Colors.grey,
+                onTap: _onItemTapped,
+                elevation: 15.0,
+                type: BottomNavigationBarType.fixed),
+          )
         ],
       ),
     );
   }
+
+  final _bottomNavItem = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      activeIcon: Icon(Icons.home),
+      icon: Icon(Icons.home_outlined),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      activeIcon: Icon(Icons.account_circle_rounded),
+      icon: Icon(Icons.account_circle_outlined),
+      label: 'Profile',
+    ),
+  ];
 }
