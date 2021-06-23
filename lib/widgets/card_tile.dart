0601f60a94cc8124fb34c98d8d23dc9cf8db87fd
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2021-06-24 00:29:37 +0700

    Minor changes to the transaction list view

--- lib/widgets/card_tile.dart
+++ lib/widgets/card_tile.dart
@@ -36,69 +36,71 @@ class _CardTileState extends State<CardTile> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 0,
+      margin: EdgeInsets.zero,
       child: ListTile(
-          onTap: () {
-            MaterialPageRoute route = MaterialPageRoute(
-                builder: (_) => OrderDetails(
-                      id: widget.id,
-                      object: widget.object,
-                    ));
-            Navigator.push(context, route);
-          },
-          onLongPress: () {
-            setState(() {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                        title: Text('Delete Transaction ?'),
-                        actions: [
-                          MaterialButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Cancel'),
-                          ),
-                          MaterialButton(
-                            onPressed: widget.onDelete,
-                            child: Text('Delete'),
-                          ),
-                        ]);
-                  });
-            });
-          },
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              imageUrl ?? 'https://via.placeholder.com/150',
-              width: 54.19,
-              height: 54.19,
-              fit: BoxFit.cover,
-            ),
+        onTap: () {
+          MaterialPageRoute route = MaterialPageRoute(
+              builder: (_) => OrderDetails(
+                    id: widget.id,
+                    object: widget.object,
+                  ));
+          Navigator.push(context, route);
+        },
+        onLongPress: () {
+          setState(() {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                      title: Text('Delete Transaction ?'),
+                      actions: [
+                        MaterialButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        MaterialButton(
+                          onPressed: widget.onDelete,
+                          child: Text('Delete'),
+                        ),
+                      ]);
+                });
+          });
+        },
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network(
+            imageUrl ?? 'https://via.placeholder.com/150',
+            width: 54.19,
+            height: 54.19,
+            fit: BoxFit.cover,
           ),
-          title: Text(
-            '#${widget.object['orderID']}' ?? 'no name',
-            style: orderBold.copyWith(fontSize: 16, color: Color(0xff181818)),
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Text(
-            widget.object['booking_date'] ?? '01-01-1999',
-            style:
-                orderRegular.copyWith(fontSize: 12, color: Color(0xffB2B2B2)),
-            overflow: TextOverflow.ellipsis,
-          ),
-          trailing: Text(
-            (widget.object['paid'] == false) ? 'Wait' : 'Paid',
-            style: (widget.object['paid'] == false)
-                ? orderBold.copyWith(fontSize: 16, color: Colors.red)
-                : orderBold.copyWith(fontSize: 16, color: Color(0xff2C6D6A)),
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(11.84),
-          )),
-      elevation: 0.4,
+        ),
+        title: Text(
+          '#${widget.object['orderID']}' ?? 'no name',
+          style: orderBold.copyWith(fontSize: 16, color: Color(0xff181818)),
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          widget.object['booking_date'] ?? '01-01-1999',
+          style: orderRegular.copyWith(fontSize: 12, color: Color(0xffB2B2B2)),
+          overflow: TextOverflow.ellipsis,
+        ),
+        trailing: Text(
+          (widget.object['paid'] == false) ? 'Wait' : 'Paid',
+          style: (widget.object['paid'] == false)
+              ? orderBold.copyWith(fontSize: 16, color: Colors.red)
+              : orderBold.copyWith(fontSize: 16, color: Color(0xff2C6D6A)),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(11.84),
+        ),
+        contentPadding: EdgeInsets.all(12),
+      ),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(3),
       ),
     );
   }
